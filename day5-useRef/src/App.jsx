325f66301d1e01c3928ecd83a9b1af99a7010938
Adil Alam <mdadil8840@gmail.com>
2026-09-13 01:42:36 +0530

    useRef concept

---
+++ day5-useRef/src/App.jsx
@@ -0,0 +1,17 @@
+import React, { useRef } from 'react'
+import Form from './Form'
+
+const App = () => {
+  const inpRef = useRef();
+   
+
+  return (
+    <div >
+      <h1>Hello Adil</h1>
+      <Form/>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ day5-useRef/src/Form.jsx
@@ -0,0 +1,65 @@
+import React, { useRef, useState } from "react";
+
+const Form = () => {
+  console.log("form rendering...");
+
+  const formRef = useRef({});
+
+  const [products, setProducts] = useState({});
+  console.log(formRef);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let obj = {
+       productName : formRef.current.productName.value,
+       price : formRef.current.price.value,
+       category : formRef.current.category.value,
+       image : formRef.current.image.value
+    }
+    setProducts(obj)
+  };
+
+  return (
+    <div className="w-80 h-screen">
+      <form
+        onSubmit={handleSubmit}
+        className="flex p-6 rounded bg-white flex-col gap-4"
+      >
+        <input
+          ref={(e) => formRef.current.productName = e}
+          className="p-2 border border-gray-400 rounded"
+          type="text"
+          placeholder="Product name"
+        />
+        <input
+          ref={(e) => formRef.current.price = e}
+
+          className="p-2 border border-gray-400 rounded"
+          type="text"
+          placeholder="Price"
+        />
+        <span>Select category:</span>
+        <select ref={(e) => formRef.current.category = e} className="p-2 border border-gray-400 rounded">
+          <option value="MENS">Mens</option>
+          <option value="WOMEN">Women</option>
+          <option value="KIDS">Kids</option>
+        </select>
+        <input
+          ref={(e) => formRef.current.image = e}
+          className="p-2 border border-gray-400 rounded"
+          type="text"
+          placeholder="image"
+        />
+        <button className="p-2 bg-blue-600 text-white rounded">CREATE</button>
+      </form>
+
+      <h1>{products.productName}</h1>
+      <h1>{products.price} </h1>
+      <h1>{products.category} </h1>
+      <h1>{products.image} </h1>
+    </div>
+  );
+};
+
+export default Form;
